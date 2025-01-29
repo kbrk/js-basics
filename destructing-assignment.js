bfa2ebf99b7b4921986f8a6822bf0045c1bd9cc6
Burak Akdemir <brakdemir@gmail.com>
2025-01-29 11:09:25 +0300

    destructing assignment

--- destructing-assignment.js
+++ destructing-assignment.js
@@ -1,18 +1,18 @@
 // Destructing assignment is a JS expression that unpacks an object properties into variables.
 
+// object into variables
 const obj = {
     a: 'objA',
     b: "objB",
     c: "objC",
-    d: "objD",
+    //d: "objD",
     e: "objE",
     f: "objF"
 }
-
-const arr = ["arrA", "arrB", "arrC", "arrD", "arrE"];
-
-let { a, b, c, d = 'D', ...rest } = obj;
-let [arrA, arrB, arrC, ...arrRest] = arr;
-
+let { a, b, c, d = 'default D', ...rest } = obj;
 console.log(a, b, c, d, rest);
+
+// array into variables
+const arr = ["arrA", "arrB", "arrC", "arrD", "arrE"];
+let [arrA, arrB, arrC, ...arrRest] = arr;
 console.log(arrA, arrB, arrC, arrRest);
